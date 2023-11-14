eb1cb1b69a915084dc8a9b938ba91a682922ebbc
ghandurathallah10 <ghandurathallah10@gmail.com>
2023-11-14 23:51:38 +0700

    setup app.jsx

--- Kasir-Project/src/App.jsx
+++ Kasir-Project/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './components/navbar';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComponent from "./components/navbar";
+import ListCategory from "./components/listCategory";
+import ListProduct from "./components/listProduct";
+import Footer from "./components/footer";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    < NavbarComponent/>
+      <NavbarComponent />
+      <Container>
+        <Row>
+          <Col>
+            <div className="px-5 mt-5">
+              <ListCategory />
+            </div>
+            <div className="px-5 mt-5">
+              <ListProduct />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Kasir-Project/src/components/footer.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Footer () {
+  return (
+    <footer className="mt-5">
+      <Container>
+        <Row>
+          <Col>
+            <p>&copy; 2023 Your Company. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
